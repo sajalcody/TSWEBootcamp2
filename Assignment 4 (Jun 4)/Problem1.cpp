8ef1e65cf3df14b202b8ebbff4b6ba9d110e554d
Sajal Agrawal <sajal.agrawal@juspay.in>
2020-06-04 23:08:50 +0530

    Assignment 4

--- Assignment 4 (Jun 4)/Problem1.cpp
+++ Assignment 4 (Jun 4)/Problem1.cpp
@@ -1,1 +1,30 @@
 // Link: https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& a, int k) {
+        int n = a.size(), cumSum = 0;
+        set<pair<int, int>> mm;
+        mm.insert({0, -1});
+        
+        int minn = INT_MAX;
+        for(int i = 0; i < n; i++) {
+            cumSum += a[i];
+            set<pair<int, int>>::iterator it = mm.insert(make_pair(cumSum, i)).first;
+            while (next(it) != mm.end()) {
+                auto ahead = next(it);
+                if (ahead->first >= it->first and ahead->second <= it->second)
+                    mm.erase(ahead);
+                else it = ahead;
+            }
+
+            auto up = mm.upper_bound(make_pair(cumSum - k, INT_MAX));
+            if (up != mm.begin()) {
+                auto before = prev(up);
+                minn = min(minn, i - before->second);    
+            }
+        }
+        
+        return minn == INT_MAX? -1: minn;
+    }
+};
--- Assignment 4 (Jun 4)/Problem2.cpp
+++ Assignment 4 (Jun 4)/Problem2.cpp
@@ -1,1 +1,24 @@
 // Link: https://leetcode.com/problems/reconstruct-itinerary/
+
+class Solution {
+public:
+    vector<string> ans;
+    void findItineraryHelper(string start, unordered_map<string, priority_queue<string, vector<string>, greater<string>>>& map) {
+    while (!map[start].empty()) {
+        string res = map[start].top();
+        map[start].pop();
+        findItineraryHelper(res, map);
+    }
+    ans.push_back(start);
+}
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        ans.clear();
+        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> map;
+        for (auto p = tickets.begin(); p != tickets.end(); p++) {
+            map[(*p)[0]].push((*p)[1]);
+        }
+        findItineraryHelper("JFK", map);
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
